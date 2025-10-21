b59964b302031c6f587d1d324e190f6f2344f3b1
AzPepoze <pladumguy@gmail.com>
2025-10-22 00:36:48 +0700

    update url

--- server.js
+++ server.js
@@ -6,48 +6,47 @@ const { MongoClient } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const uri = process.env.MONGO_URI;
+const mongo_url = process.env.MONGO_URL;
 const dbName = process.env.DB_NAME;
 const collectionName = process.env.COLLECTION_NAME;
 
 let client;
 
 async function connectToMongo() {
-    console.log('Attempting to connect to MongoDB...');
-    try {
-        client = new MongoClient(uri);
-        await client.connect();
-        console.log('Successfully connected to MongoDB.');
-    } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
-        process.exit(1);
-    }
+	console.log("Attempting to connect to MongoDB...");
+	try {
+		client = new MongoClient(mongo_url);
+		await client.connect();
+		console.log("Successfully connected to MongoDB.");
+	} catch (err) {
+		console.error("Failed to connect to MongoDB", err);
+		process.exit(1);
+	}
 }
 
 app.get("/", async (req, res) => {
-    try {
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
+	try {
+		const db = client.db(dbName);
+		const collection = db.collection(collectionName);
 
-        const result = await collection.findOneAndUpdate(
-            { name: "visits" },
-            { $inc: { count: 1 } },
-            { upsert: true, returnDocument: "after" }
-        );
+		const result = await collection.findOneAndUpdate(
+			{ name: "visits" },
+			{ $inc: { count: 1 } },
+			{ upsert: true, returnDocument: "after" }
+		);
 
-        console.log(`Visit count: ${result.value.count}`);
-    } catch (err) {
-        console.error("Failed to update visit count", err);
-        res.status(500).send("Failed to update visit count");
-    } finally {
-    }
+		console.log(`Visit count: ${result.value.count}`);
+	} catch (err) {
+		console.error("Failed to update visit count", err);
+		res.status(500).send("Failed to update visit count");
+	} finally {
+	}
 
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+	res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-
 connectToMongo().then(() => {
-    app.listen(port, () => {
-        console.log(`Server listening at http://localhost:${port}`);
-    });
+	app.listen(port, () => {
+		console.log(`Server listening at http://localhost:${port}`);
+	});
 });
